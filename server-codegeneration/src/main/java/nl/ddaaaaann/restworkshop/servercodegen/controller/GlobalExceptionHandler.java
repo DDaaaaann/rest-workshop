2ce19e08a0e84f3891d645cd38ddcb2df31fcaa4
Daan de Graaf <d.degraaf@dictu.nl>
2024-10-08 09:59:07 +0200

    Update exceptionhandler

--- server-codegeneration/src/main/java/nl/ddaaaaann/restworkshop/servercodegen/controller/GlobalExceptionHandler.java
+++ server-codegeneration/src/main/java/nl/ddaaaaann/restworkshop/servercodegen/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package nl.ddaaaaann.restworkshop.servercodegen.controller;
 
 import nl.ddaaaaann.restworkshop.servercodegen.exception.GuestNotFoundException;
 import nl.ddaaaaann.restworkshop.servercodegen.exception.ReservationNotFoundException;
+import nl.ddaaaaann.restworkshop.servercodegen.exception.RoomNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler({GuestNotFoundException.class, ReservationNotFoundException.class})
+  @ExceptionHandler({GuestNotFoundException.class, ReservationNotFoundException.class, RoomNotFoundException.class})
   public ResponseEntity<String> handleNotFoundException(GuestNotFoundException ex) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
         .body(ex.getMessage());
