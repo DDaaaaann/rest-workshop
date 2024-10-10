eb599d7047c0cd782df80717068b770af03c20bc
Daan de Graaf <d.degraaf@dictu.nl>
2024-10-10 17:34:15 +0200

    Clear implementations

--- server-codegeneration/src/main/java/nl/ddaaaaann/restworkshop/servercodegen/mapper/GuestMapper.java
+++ server-codegeneration/src/main/java/nl/ddaaaaann/restworkshop/servercodegen/mapper/GuestMapper.java
@@ -1,18 +1,9 @@
 package nl.ddaaaaann.restworkshop.servercodegen.mapper;
 
-import nl.ddaaaaann.rest.hotel.model.GuestCreateDto;
-import nl.ddaaaaann.rest.hotel.model.GuestDto;
-import nl.ddaaaaann.rest.hotel.model.GuestUpdateDto;
-import nl.ddaaaaann.restworkshop.servercodegen.model.Guest;
 import org.mapstruct.Mapper;
 
 
 @Mapper(componentModel = "spring")
 public interface GuestMapper {
-  
-  GuestDto toDto(Guest model);
-  
-  Guest toModel(GuestCreateDto dto);
-
-  Guest toModel(GuestUpdateDto dto);
+//  TODO: Implement mappers
 }
